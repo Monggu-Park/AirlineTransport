979ad9da68b68e4cc2735a5abf2b6ba4947caa69
Monggu-Park <tomas407@naver.com>
2024-11-05 15:57:09 +0900

    inital kotilin server

---
+++ src/main/kotlin/com/buddamanse/seairline/SeAirlineApplication.kt
@@ -0,0 +1,11 @@
+package com.buddamanse.seairline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeAirlineApplication
+
+fun main(args: Array<String>) {
+    runApplication<SeAirlineApplication>(*args)
+}
